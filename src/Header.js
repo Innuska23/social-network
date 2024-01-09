d55e462375a6ce60b0957174af87959fc8fa5f93
Inna Dmytrenko <inna.shalun@gmail.com>
2024-01-09 14:53:57 +0200

    Initialize project using Create React App

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <div>
+        <a href="#home">Home-</a>
+        <a href="news">News Feed-</a>
+        <a href="messageS">Messages-</a>
+      </div>
+    );
+  };
+
+export default Header;
